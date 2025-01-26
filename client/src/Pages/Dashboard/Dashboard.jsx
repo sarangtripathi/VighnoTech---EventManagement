3205bf303c41dbe9cb71b349466ce06a466482a9
Vivek Yadav <yadav7738vivek@gmail.com>
2025-01-27 02:28:27 +0530

    authorization

--- client/src/Pages/Dashboard/Dashboard.jsx
+++ client/src/Pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useEffect,useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import 'react-toastify/dist/ReactToastify.css';
+import  {ToastContainer,toast }from "react-toastify"
+// import { handleError, handleSuccess } from '../utils';
 import Footer from '../../components/Layout/Footer.jsx'
 
 const DashBoard = () => {
+  const [loggedInUser,setLoggedInUser]=useState();
+  const [products,setProducts]=useState([]);
+  const navigate=useNavigate();
+  
+  useEffect(()=>{
+        setLoggedInUser(localStorage.getItem('loggedInUser'))
+  },[])
+
+  const handleLogout=(e)=>{
+    localStorage.removeItem('token');
+    localStorage.removeItem('loggedInUser');
+    // handleSuccess('User LoggedOut')
+    setTimeout(()=>{
+         navigate('/login');
+    },1000)
+
+  } 
   return (
     <div>DashBoard</div>
    
--- client/src/components/Layout/Header.jsx
+++ client/src/components/Layout/Header.jsx
@@ -1,8 +1,30 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
+import React, { useEffect,useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import 'react-toastify/dist/ReactToastify.css';
+import  {ToastContainer,toast }from "react-toastify"
+// import { handleError, handleSuccess } from '../utils';
 // import { useAuth } from '../../contexts/AuthContext';
 
 const Header = () => {
+  const [loggedInUser,setLoggedInUser]=useState();
+  const [products,setProducts]=useState([]);
+  const navigate=useNavigate();
+  
+  useEffect(()=>{
+        setLoggedInUser(localStorage.getItem('loggedInUser'))
+  },[])
+
+
+  const handleLogout=(e)=>{
+    localStorage.removeItem('token');
+    localStorage.removeItem('loggedInUser');
+    toast.success('User LoggedOut')
+    setTimeout(()=>{
+         navigate('/login');
+    },1000)
+
+  }
 //   const { user, logoutUser } = useAuth();
 
   return (
@@ -20,10 +42,11 @@ const Header = () => {
             </>
           {/* ) : ( */}
             <>
-              <Link to="/login" className="mr-4 hover:underline">Login</Link>
-              <Link to="/register" className="bg-green-500 hover:bg-green-600 px-4 py-2 rounded">
+              {/* <Link to="/login" className="mr-4 hover:underline">Login</Link> */}
+              <button  className="ml-4 border rounded px-3 py-1 bg-red-400 font-semibold" onClick={handleLogout}>Logout</button>
+              {/* <Link to="/register" className="bg-green-500 hover:bg-green-600 px-4 py-2 rounded">
                 Register
-              </Link>
+              </Link> */}
             </>
           {/* )} */}
         </nav>
