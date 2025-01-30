fd6a0814a168f18c3e43c59b587a562baae75c13
Sarang Tripathi <sarangtripathi18@gmail.com>
2025-01-31 02:07:27 +0530

    added frontend link

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ dotenv.config({
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://vighno-tech-event-management-1fai-j7evmgiw0.vercel.app"],
     credentials: true,
   })
 );
